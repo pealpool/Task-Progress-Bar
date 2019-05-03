c6c120c3737bceca7652b820ac2f5109b7f2762a
pealpool <pealpool@gmail.com>
2019-05-04 00:05:43 +0800

    go

---
+++ Task-Progress-Bar/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+namespace Task_Progress_Bar
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void TMwindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            double wWidth = System.Windows.Forms.SystemInformation.WorkingArea.Width;
+            double wHeight = System.Windows.Forms.SystemInformation.WorkingArea.Height;
+            TMwindow.Width = wWidth;
+            TMwindow.Top = wHeight - 10;
+            TMwindow.Left = 0;
+            myGrid.Width = wWidth;
+            myProBar.Width = wWidth;
+            InitNotifyIcon();
+        }
+        private void InitNotifyIcon()
+        {
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon();
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.Text = "单击设置";
+            this.notifyIcon.Icon = System.Drawing.Icon.ExtractAssociatedIcon(System.Windows.Forms.Application.ExecutablePath);
+            //打开菜单项
+            System.Windows.Forms.MenuItem open = new System.Windows.Forms.MenuItem("Open");
+            open.Click += new EventHandler(Show);
+            //退出菜单项
+            System.Windows.Forms.MenuItem exit = new System.Windows.Forms.MenuItem("Exit");
+            exit.Click += new EventHandler(Close);
+            //关联托盘控件
+            System.Windows.Forms.MenuItem[] childen = new System.Windows.Forms.MenuItem[] { open, exit };
+            notifyIcon.ContextMenu = new System.Windows.Forms.ContextMenu(childen);
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler((o, e) =>
+            {
+                if (e.Button == System.Windows.Forms.MouseButtons.Left) this.Show(o, e);
+            });
+        }
+        private void Show(object sender, EventArgs e)
+        {
+            this.Visibility = System.Windows.Visibility.Visible;
+            this.ShowInTaskbar = true;
+            this.Activate();
+        }
+        private void Hide(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = false;
+            this.Visibility = System.Windows.Visibility.Hidden;
+        }
+        private void Close(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = false;
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
