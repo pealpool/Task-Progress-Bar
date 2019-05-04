835a5e27a7a9f3dc0f3914d46fd274e3db3166fd
pealpool <pealpool@gmail.com>
2019-05-04 17:01:14 +0800

    33

---
+++ Task-Progress-Bar/help/ColorPicker2010_src/ColorPickerDemo/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace ColorPickerDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBlock_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
--- Task-Progress-Bar/setWindow.xaml.cs
+++ Task-Progress-Bar/setWindow.xaml.cs
@@ -24,9 +24,102 @@ namespace Task_Progress_Bar
             InitializeComponent();
         }
 
-        private void setWinLoaded(object sender, RoutedEventArgs e)
+        private void SetWinLoaded(object sender, RoutedEventArgs e)
         {
 
         }
+
+        private void TextBox_se1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //TextBox txt = sender as TextBox;
+            MyKeyDown(textBox_se1,e)
+        }
+        private void MyKeyDown(TextBox sender,KeyEventArgs e)
+        {
+            if ((e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9))
+            {
+                e.Handled = false;
+            }
+            else if (((e.Key >= Key.D0 && e.Key <= Key.D9)))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+        private void TextBox_se1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Xianzhi(textBox_se1);
+        }
+        private void TextBox_se2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9))
+            {
+                e.Handled = false;
+            }
+            else if (((e.Key >= Key.D0 && e.Key <= Key.D9)))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+        private void TextBox_se2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Xianzhi(textBox_se2);
+        }
+
+        private void TextBox_mi1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9))
+            {
+                e.Handled = false;
+            }
+            else if (((e.Key >= Key.D0 && e.Key <= Key.D9)))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+        private void TextBox_mi1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Xianzhi(textBox_mi1);
+        }
+
+        //限制最大数值
+        private void Xianzhi(TextBox sender) {
+            try
+            {
+                string strNum = sender.Text;
+                if ("" == strNum || null == strNum)
+                {
+                    return;
+                }
+                int num = int.Parse(sender.Text);
+                sender.Text = num.ToString();
+                if (num <= 9999)
+                {
+                    return;
+                }
+                else
+                {
+                    //sender.Text = "";
+                    sender.Text = sender.Text.Remove(2);
+                    sender.SelectionStart = sender.Text.Length;
+                }
+            }
+            catch
+            {
+                sender.Text = "";
+            }
+        }
+
     }
 }
